32eaec1a667201c709b7d2f0a4e392f78d62c19a
Mohammed Abdelmoneim <mohammed.abdelmoneim@ebanan.local>
2019-08-26 21:17:53 +0200

    finishing up with google authentication

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb://uhrkw4xw19iwrgenog5r:C4sMAs6YGDO4h2XyCrn8@bvqucvkuvzopuyf-mongodb.services.clever-cloud.com:27017/bvqucvkuvzopuyf",
+    externalMongoURI: "mongodb://uhrkw4xw19iwrgenog5r:C4sMAs6YGDO4h2XyCrn8@bvqucvkuvzopuyf-mongodb.services.clever-cloud.com:27017/bvqucvkuvzopuyf",
+    internalMongoURI: "mongodb://localhost:27017/storybooks",
     googleClientID:"725620443523-7q5su6s3gdhlvhap972ariek1uk7glo2.apps.googleusercontent.com",
-    googleCLientSecret:"LyACsz8WjkgTxe00V6Rce_qz"
+    googleClientSecret:"LyACsz8WjkgTxe00V6Rce_qz"
 }
